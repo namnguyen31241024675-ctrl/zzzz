d663dbb84e17bc102fd544c65f1ed91ca61fa174
namnguyen31241024675-ctrl <namnguyen.31241024675@st.ueh.edu.vn>
2025-09-22 10:03:50 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Khai báo biến
+        string name;
+        
+        // Nhập tên từ người dùng
+        Console.Write("Nhập tên của bạn: ");
+        name = Console.ReadLine();
+        
+        // In ra tên đã nhập
+        Console.WriteLine("Chào " + name + "!");
+    }
+}
